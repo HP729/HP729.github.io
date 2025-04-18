99ff1919f7141689d6ec7d98472b701c209b2433
HP729 <103089702+HP729@users.noreply.github.com>
2025-04-17 19:57:33 -0700

    Add files via upload

---
+++ first-website/scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/UW.png") {
+    myImage.setAttribute("src", "images/UW2.png");
+  } else {
+    myImage.setAttribute("src", "images/UW.png");
+  }
+});
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `U r da g, ${myName}`;
+  }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Hello, ${storedName}`;
+}
